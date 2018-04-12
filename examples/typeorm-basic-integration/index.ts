8db0cc8462185a22fb25ce162211b9fb6b01b1ee
Adam <adam@pietrasiak.com>
2018-04-12 14:20:05 +0200

    Improved typeorm integration example

--- examples/typeorm-basic-integration/index.ts
+++ examples/typeorm-basic-integration/index.ts
@@ -1,9 +1,29 @@
 import * as express from 'express';
-import { Entity, BaseEntity } from 'typeorm';
+import { Entity, BaseEntity, ManyToOne, OneToMany } from 'typeorm';
 import { Schema, Query, Mutation, ObjectType, Field, compileSchema } from 'typegql';
 import * as graphqlHTTP from 'express-graphql';
 
 import { PrimaryGeneratedColumn, Column, createConnection } from 'typeorm';
+
+@Entity()
+@ObjectType()
+export class Book extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  @Field()
+  id: number;
+
+  @Column()
+  @Field()
+  title: string;
+
+  @Column()
+  @Field()
+  pagesCount: number;
+
+  @ManyToOne(type => User, user => user.books, { lazy: true })
+  @Field({ type: () => User })
+  author: User;
+}
 
 @Entity()
 @ObjectType()
@@ -19,6 +39,10 @@ export class User extends BaseEntity {
   @Column()
   @Field()
   age: number;
+
+  @OneToMany(type => Book, book => book.author)
+  @Field({ type: () => [Book] })
+  books: Book[];
 
   @Field()
   isAdult(): boolean {
@@ -40,10 +64,22 @@ class ApiSchema {
     return user;
   }
 
+  @Query({ type: [Book] })
+  async getAllBooks(): Promise<Book[]> {
+    const books = await Book.find();
+    return books;
+  }
+
   @Mutation({ type: User })
   async createUser(name: string, age: number): Promise<User> {
     const newUser = User.create({ age, name });
     return await newUser.save();
+  }
+
+  @Mutation({ type: Book })
+  async createBook(title: string, pagesCount: number, authorId: number): Promise<Book> {
+    const newBook = Book.create({ title, pagesCount, author: { id: authorId } });
+    return await newBook.save();
   }
 }
 
@@ -60,7 +96,7 @@ async function startApp() {
     username: 'adam',
     password: '',
     database: 'test',
-    entities: [User],
+    entities: [User, Book],
     synchronize: true,
   });
   console.log('Connected');
