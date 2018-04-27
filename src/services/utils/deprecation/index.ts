473fbbcb6f66670562ff8da9c5e8586670866295
Adam <adam@pietrasiak.com>
2018-04-27 10:57:52 +0200

    Added deprecation test

--- src/services/utils/deprecation/index.ts
+++ src/services/utils/deprecation/index.ts
@@ -1,6 +1,10 @@
 const shownRegistry = new WeakMap<any, true>();
 
-export function showDeprecationWarning(message: string, uniqueIdentifier?: any) {
+export function showDeprecationWarning(
+  message: string,
+  uniqueIdentifier?: any,
+  callback?: (message: string) => void,
+) {
   if (uniqueIdentifier && shownRegistry.has(uniqueIdentifier)) {
     return;
   }
@@ -8,4 +12,7 @@ export function showDeprecationWarning(message: string, uniqueIdentifier?: any) 
     shownRegistry.set(uniqueIdentifier, true);
   }
   console.warn(`@Deprecation warning: ${message}`);
+  if (callback) {
+    callback(message);
+  }
 }
