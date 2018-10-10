127001bd61a40b80466ebe621698bc5a32dc0e79
Jiri Spac <capajj@gmail.com>
2018-10-10 11:34:00 +0200

    fixes #10

--- src/test/functional/inputObjectTypeAsInstance.spec.ts
+++ src/test/functional/inputObjectTypeAsInstance.spec.ts
@@ -8,6 +8,7 @@ import {
   compileSchema,
   InputObjectType,
   InputField,
+  Mutation,
 } from '../..'
 
 @InputObjectType()
@@ -16,7 +17,7 @@ class Input {
   inputField: string
 
   someMethodOnInputObjectType() {
-    return 'works'
+    return 'works' + this.inputField
   }
 }
 
@@ -26,6 +27,14 @@ class Hello {
   bar(@Arg({ type: Input }) input: Input): string {
     return input.someMethodOnInputObjectType()
   }
+  @Field()
+  barWithList(@Arg({ type: [Input] }) inputs: Input[]): string {
+    return inputs
+      .map((input) => {
+        return input.someMethodOnInputObjectType()
+      })
+      .join('|')
+  }
 }
 
 @SchemaRoot()
@@ -34,23 +43,76 @@ class FooSchema {
   hello(): Hello {
     return new Hello()
   }
+
+  @Mutation()
+  barMutation(@Arg({ type: Input }) input: Input): string {
+    return input.someMethodOnInputObjectType()
+  }
 }
 
 const schema = compileSchema({ roots: [FooSchema] })
 
 describe('input object type arguments are passed as instances', () => {
   it('passes as Input', async () => {
-    const result = await graphql(
+    const result1 = await graphql(
       schema,
       `
         {
           hello {
-            bar(input: { inputField: "Foob" })
+            bar(input: { inputField: "Foo" })
           }
         }
       `,
     )
 
-    expect(result).toMatchSnapshot()
+    expect(result1).toMatchInlineSnapshot(`
+Object {
+  "data": Object {
+    "hello": Object {
+      "bar": "worksFoo",
+    },
+  },
+}
+`)
+
+    const result2 = await graphql(
+      schema,
+      `
+        {
+          hello {
+            barWithList(
+              inputs: [{ inputField: "Foo1" }, { inputField: "Foo2" }]
+            )
+          }
+        }
+      `,
+    )
+
+    expect(result2).toMatchInlineSnapshot(`
+Object {
+  "data": Object {
+    "hello": Object {
+      "barWithList": "worksFoo1|worksFoo2",
+    },
+  },
+}
+`)
+
+    const result3 = await graphql(
+      schema,
+      `
+        mutation {
+          barMutation(input: { inputField: "Bar" })
+        }
+      `,
+    )
+
+    expect(result3).toMatchInlineSnapshot(`
+Object {
+  "data": Object {
+    "barMutation": "worksBar",
+  },
+}
+`)
   })
 })
