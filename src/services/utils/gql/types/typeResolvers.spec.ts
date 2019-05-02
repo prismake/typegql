5abad8dfa407065cff8bec3494c840d5015fc8e8
Jiří Špác <capajj@gmail.com>
2019-05-02 23:04:49 +0200

    update dev deps and add one small spec

---
+++ src/services/utils/gql/types/typeResolvers.spec.ts
@@ -0,0 +1,11 @@
+import { resolveType } from './typeResolvers'
+
+describe('resolveType', function() {
+  it('should resolve an array type', () => {
+    expect(resolveType([Number])).toMatchInlineSnapshot(`"[Float!]"`)
+  })
+
+  it('should resolve a thunk returning an array type', () => {
+    expect(resolveType(() => [Number])).toMatchInlineSnapshot(`"[Float!]"`)
+  })
+})
